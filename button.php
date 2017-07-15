58398d986b5a83b12ad8dfcbe2441fcba90bebd9
welcometriculin <gowtham.a@triculintech.com>
2017-07-15 10:43:09 +0530

    payumoney integration

---
+++ button.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<div class="pm-button">
+		<a href="https://test.payumoney.com/paybypayumoney/#/994">
+			<img src="https://test.payumoney.com//media/images/payby_payumoney/buttons/211.png" />
+		</a>
+	</div>
+</body>
+</html>
